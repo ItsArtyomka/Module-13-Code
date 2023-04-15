af02d047d75e4951c627a0d52b203ba8ae02bc63
Artyom Pionov <pionovartem11@gmail.com>
2023-04-15 22:35:19 +0100

    First Commit: Starting to learn about Thread class. Also worked on multi-client mountain echo app, done by creating server and connecting using client. Very cool. Made a few comments, still more needed.

--- src/NetworkEcho/EchoServer.java
+++ src/NetworkEcho/EchoServer.java
@@ -13,20 +13,21 @@ public class EchoServer {
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(1234);
         while(true) {
+            // Waiting for client.
             System.out.println("Waiting...");
             Socket socket = server.accept();
             System.out.println("Client connected!");
 
-            // Can write like that
+            // Can write Server-client stuff this way.
 //            Client client = new Client(socket);
 //            Thread thread = new Thread(client);
 //            thread.start();
 
-            //--OR--
+            // OR this way.
 //            Thread thread = new Thread (new Client(socket));
 //            thread.start();
 
-            //--OR--
+            // OR this way.
             new Thread (new Client(socket)).start();
         }
     }
